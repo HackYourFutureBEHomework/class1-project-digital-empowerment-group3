52b5c531a4735df7645fcce7c14fb71a7c4db1e8
mahmoudlb1 <39495074+mahmoudlb1@users.noreply.github.com>
2018-11-11 08:30:06 +0100

    Update module.controller.js
    new add

--- server/src/controller/module.controller.js
+++ server/src/controller/module.controller.js
@@ -9,3 +9,12 @@ exports.findAll = (req, res) => {
       });
     });
 };
+exports.create=(req,res) => {
+  const newMoudle =new Module(req.body);
+  newModule
+  save()
+  then((date)=>{res.send(data); })
+  catch((err) =>{
+    res.status(500).send({message:err.message });
+  });
+};
