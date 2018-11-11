aafe67b1c8cc8cee0b434413caa11a1ac2ebe90e
eduardo hernandez <eduardohralejandro@gmail.com>
2018-11-11 01:45:46 +0100

    task:added new modules to the database

--- client/src/api/modules.js
+++ client/src/api/modules.js
@@ -3,3 +3,15 @@ const API_URL = 'http://localhost:4000'
 export const getModules = () => {
   return fetch(`${API_URL}/module`).then(response => response.json());
 };
+
+export const createModule = (title) => {
+  return fetch(`${API_URL}/module`, {
+    method: 'POST',
+    headers: new Headers({
+        "Content-Type": "application/json"
+      }),
+    body: JSON.stringify({
+      title: title,
+    })
+  }).then(response => response.json());
+};
--- client/src/components/Modules.jsx
+++ client/src/components/Modules.jsx
@@ -1,32 +1,93 @@
 import React, { Component} from 'react';
 import { getModules } from '../api/modules';
-
+import { createModule } from '../api/modules';
 class Modules extends Component {
   state = {
-    modules: []
-  };
+  title:"",
+  modules: []
+};
 
-  componentDidMount() {
-    getModules().then((modules) => {
-      this.setState({ modules: modules });
-    });
+ componentDidMount = () => {
+    getModules()
+    .then((modules) => {
+        this.setState({modules: modules});
+     });
   }
 
+  handleChange = (e) =>{
+    this.setState({title: e.target.value});
+  }
+  handleSubmit = () => {
+      createModule(this.state.title)
+      .then(newTitle => {
+         this.setState({
+          title: [...this.state.title, newTitle],
+        });
+      })
+  }
+  
   render() {
-    const { modules } = this.state;
-
-    if (modules.length > 0) {
-      return (
-        <ul>
-          {modules.map((module) => <li key={module._id}>{module.title}</li>)}
-        </ul>
-      )
-    } else {
-      return (
-        <p>There are no modules yet</p>
-      )
-    }
-  }
+     return(
+        <div>
+        <input type="text" 
+            value={this.state.title}
+            onChange={this.handleChange}/>
+            <button onClick={this.handleSubmit}>add todo</button>
+        </div>
+     )
+     }
 }
-
 export default Modules;
+
+//  <form onSubmit={this.handleAddModule}>
+//               <input type="text" name="option" placeholder="enter your new module"/>
+//               <button>Add Module</button>
+//   </form>  
+
+//  if(modules.length >= 0){
+  //     return(
+  //       <div>
+  //        <form onSubmit={this.handleAddModule}>
+  //         <input type="text" name="option" />
+  //         <button>Add Module</button>
+  //       </form>
+  //          <p>{this.state.title}</p>
+  //         {modules.map((module) => <div key={module._id}>{module.title}</div>)} 
+  //       </div>
+  //     )
+  //   } else {
+  //     return (
+  //       <p>There are no modules yet</p>
+  //     )
+  //   }
+    
+  // }
+
+
+
+ 
+
+ 
+
+  // handleAddModule = (e) => {
+  //    e.preventDefault();
+  //    let title = e.target.elements.option.value
+  //     createModule(this.state.modules).then(module => {
+  //     this.setState({
+  //       modules: this.state.modules,
+  //       title: title
+  //     });
+  //   });
+
+
+  //  handleAddModule = event => {
+//   event.preventDefault();
+//       let module = event.target.elements.option.value.trim();
+//         console.log(module)
+//       this.setState({
+//       modules: module,    
+//       }); 
+//   if (module) {
+//       event.target.elements.option.value = "";
+//     }   
+//   }
