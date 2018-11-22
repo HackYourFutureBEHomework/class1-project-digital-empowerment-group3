52597d7b72a62c8bfc533191c5093eb126ee6c30
MahmoudAbdo90 <mahmoud.mostafa25@gmail.com>
2018-11-22 16:06:38 +0100

    edit server

--- server/src/model/module.model.js
+++ server/src/model/module.model.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose');
 
 const ModuleSchema = mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true
-    },
-    explanation: String,
-    exercise: String,
-    evaluation: String,
-  },
-  {
-    timestamps: true
-  }
+	{
+		title: {
+			type: String,
+			required: true
+		},
+		title2: String,
+		title3: String,
+		explanation: String,
+		exercise: String,
+		evaluation: String
+	},
+	{
+		timestamps: true
+	}
 );
 
 module.exports = mongoose.model('Module', ModuleSchema);
