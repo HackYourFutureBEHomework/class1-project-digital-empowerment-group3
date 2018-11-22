f1cd6dd1eab0179fdb28fc17fd0a9ac1d96ea930
mahmoudlb1 <39495074+mahmoudlb1@users.noreply.github.com>
2018-11-22 21:29:03 +0100

    Update module.route.js

--- server/src/route/module.route.js
+++ server/src/route/module.route.js
@@ -2,5 +2,7 @@ const modules = require('../controller/module.controller.js');
 
 module.exports = (app) => {
   app.get('/module', modules.findAll);
-  app.post('/module',modules.create);
+  app.post('/module', modules.create);
+  app.delete('/module/:id', modules.destroy);
+  app.put('/module/:id', modules.update);
 };
