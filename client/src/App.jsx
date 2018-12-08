d385795e568ec113b7506fb108b62aeb6db920b9
mahmoudlb1 <39495074+mahmoudlb1@users.noreply.github.com>
2018-12-08 23:28:03 +0100

    Update App.jsx
    correct App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, {Component } from 'react';
+import { BrowserRouter as Route, Switch ,Redirect} from 'react-router-dom';
+import Paths from './components/PathsHome ';
 import Modules from './components/Modules';
+import Login from './components/Login';
+import Cookies from 'universal-cookie';
 
-const App = () => (
-	<BrowserRouter>
-		<Switch>
-			<Route path=":path(modules|)" component={Modules} />
-			<Route render={() => <p>Page not found</p>} />
-		</Switch>
-	</BrowserRouter>
-);
+const cookies = new Cookies();
+class App extends Component {
+	constructor() {
+		super();
+    
+    const token = cookies.get('token');
+    this.state={
+      loggedIn:!!token
+    };
+  }
+  setLoggedInState = () => {
+  this.setState({ loggedIn:true });
+  };
 
-<<<<<<< HEAD
+render() {
+  const { loggedIn } = this.state;
+		console.log(loggedIn);
+		return (
+			<Router>
+				<Switch>
+					<Route exact path="/login" render={() => <Login setLoggedInState={this.setLoggedInState} />} />
+					<Route exact path="/:path(|paths|path|index)" render={() => <Paths IsloggedIn={loggedIn} />} />
+					<Route
+						exact
+						path="/path/:pathId"
+						render={(props) => <Modules {...props} IsloggedIn={loggedIn} />}
+					/>
+				</Switch>
+			</Router>
+		);
+	}
+}
 export default App;
- 
-=======
-export default App;
->>>>>>> 4b1beb7c937490a9b7975242cf6af0770ca3294f
+
+  
