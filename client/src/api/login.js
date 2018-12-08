de9f9b0a3009107291e404d4fde3fd4d483ac405
mahmoudlb1 <39495074+mahmoudlb1@users.noreply.github.com>
2018-12-08 08:04:01 +0100

    backend working

---
+++ client/src/api/login.js
@@ -0,0 +1,15 @@
+const API_URL = 'http://localhost:4000';
+
+export const logIn = (email, password) => {
+	return fetch(`${API_URL}/path`, {
+		method: 'POST',
+		headers: new Headers({
+			'Content-Type': 'application/json'
+		}),
+
+		body: JSON.stringify({
+			email: email,
+			password: password
+		})
+	}).then((response) => response.json());
+};
