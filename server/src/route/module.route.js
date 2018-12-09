ac965a6d6dde24ddd5b6b64fcdb5ff75e9cfd983
mahmoudlb1 <39495074+mahmoudlb1@users.noreply.github.com>
2018-12-09 08:32:38 +0100

    Update module.route.js

--- server/src/route/module.route.js
+++ server/src/route/module.route.js
@@ -4,10 +4,5 @@ module.exports = (app) => {
   app.get('/module', modules.findAll);
   app.post('/module', modules.create);
   app.delete('/module/:id', modules.destroy);
-<<<<<<< HEAD
   app.put('/module/:id', modules.update);
 };
-=======
-  app.patch('/module/:id', modules.update);
-};
->>>>>>> 4b1beb7c937490a9b7975242cf6af0770ca3294f
